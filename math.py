2654191344d83d9f21ec5884058b403ff29c4bec
sharanabasavas546 <sharanabasavas546@gmail.com>
2025-10-06 12:12:54 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+addition = num1 + num2
+subtraction = num1 - num2
+
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+
+
